e950c1f953b0b689154641afe6f9e68674bb495d
Ady119 <77009197+Ady119@users.noreply.github.com>
2023-11-29 18:44:23 +0000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY = 'd@joa&da1Tg1sfj031823e0dk(*&^D)'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DATABASE = 'database/speak_it.db'
